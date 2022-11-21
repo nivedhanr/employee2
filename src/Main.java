9843a0e2c250a6691a5299896a79a9c2a34da8b1
nivedhanr <nivedharenahan@gmail.com>
2022-11-21 15:21:18 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        Scanner s=new Scanner(System.in);
+        System.out.println("1.Add employee\n2.View employee\n3.Search employee\n4.Delete employee\n5.Exit");
+        System.out.println("Enter your choice:");
+        a=s.nextInt();
+
+
+    }
+}
